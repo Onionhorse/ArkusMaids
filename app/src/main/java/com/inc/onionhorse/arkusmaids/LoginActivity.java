52fb82a0db5fefcaaef989ab0db194e56c9a28ff
Josue Ojeda Juarez <josue.ojeda12@gmail.com>
2019-04-18 03:09:38 -0700

    Initial commit Actividades de Registro, Inicio de Sesion y prueba de perfil realizado

---
+++ app/src/main/java/com/inc/onionhorse/arkusmaids/LoginActivity.java
@@ -0,0 +1,103 @@
+package com.inc.onionhorse.arkusmaids;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonSignin;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignUp;
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null){
+            //actividad de perfil va aqui
+            finish();//termina la actividad actual para brincar a Perfil
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+        buttonSignin = (Button)findViewById(R.id.buttonSignin);
+        textViewSignUp = (TextView)findViewById(R.id.textViewSignUp);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonSignin.setOnClickListener(this);
+        textViewSignUp.setOnClickListener(this);
+
+    }
+
+    private void userLogin(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+            //Correo esta vacio
+            Toast.makeText(this, "Porfavor ingrese correo", Toast.LENGTH_SHORT).show();
+            //pone alto a la ejecucion
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)){
+            //contraseña esta vacia
+            Toast.makeText(this, "Porfavor ingrese contraseña", Toast.LENGTH_SHORT).show();
+            //pone alto a la ejecucion
+            return;
+        }
+        //si la contraseña y usuario no estan vacias
+        // despliega dialogo de progreso
+        progressDialog.setMessage("Registrando usuario ...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()){
+                            //da comienzo la actividad de perfil
+                            finish();//termina la actividad actual para brincar a Perfil
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonSignin){
+            userLogin();
+        }
+        if (view == textViewSignUp){
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+    }
+}
---
+++ app/src/main/java/com/inc/onionhorse/arkusmaids/MainActivity.java
@@ -0,0 +1,113 @@
+package com.inc.onionhorse.arkusmaids;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null){
+            //actividad de perfil va aqui
+            finish();//termina la actividad actual para brincar a Perfil
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        //Inicia vistas
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignin = (TextView) findViewById(R.id.textViewSignIn);
+
+        //Agrega listener a botones y inicio de sesion
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+            //Correo esta vacio
+            Toast.makeText(this, "Please enter email adress", Toast.LENGTH_SHORT).show();
+            //pone alto a la ejecucion
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)){
+            //contraseña esta vacia
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            //pone alto a la ejecucion
+            return;
+        }
+        //si la contraseña y usuario no estan vacias
+        // despliega dialogo de progreso
+        progressDialog.setMessage("Registering User ...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                                finish();//termina la actividad actual para brincar a Perfil
+                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+//                            //usuario logro registrarse y conectarse
+//                            Toast.makeText(MainActivity.this, "Registry was succesfull", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, "Registry failed, try again please", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister){
+            registerUser();
+        }
+        if (view == textViewSignin){
+            //se abre la actividad para iniciar sesion aqui
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+    }
+}
