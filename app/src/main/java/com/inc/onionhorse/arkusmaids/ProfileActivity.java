6470ac13fb1a84419a1546979be5f936b300d706
Josue Ojeda Juarez <josue.ojeda12@gmail.com>
2019-04-19 10:34:13 -0700

    Log out funcional

--- app/src/main/java/com/inc/onionhorse/arkusmaids/ProfileActivity.java
+++ app/src/main/java/com/inc/onionhorse/arkusmaids/ProfileActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,11 +21,26 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private TextView textViewUserEmail;
     private Button buttonLogout;
 
+    private DatabaseReference databaseReference;
+
+    private EditText editTextName, editTextAdress;
+    private Button buttonSave;
+
+    //Lista
+    public ListView listView;
+    String ServiciosOpcionales[] = {"Limpieza", "Cocina"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState); setContentView(R.layout.activity_profile);
+//        listView = (ListView)findViewById(R.id.listServicios);
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_profile,R.id.textViewMensaje, ServiciosOpcionales);
+//        listView.setAdapter(arrayAdapter);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -42,6 +62,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View view) {
-
+        if (view == buttonLogout){
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
